3cbe6ef50af89ebb0c5ba88a3667f1bff611548f
raj <rpande400@gmail.com>
2023-06-13 13:48:01 +0530

    ISSUE-TRACKER

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-
+require("dotenv").config();
 const express = require('express');
 
 const db = require('./config/mongoose');
-const port = process.env.PORT || 8000;
+const port = process.env.PORT;
 const app = express();
 const path = require('path');
 const expressLayouts = require('express-ejs-layouts');
--- models/issue.js
+++ models/issue.js
@@ -1,3 +1,4 @@
+
 const mongoose = require('mongoose');
 
 const issueSchema = new mongoose.Schema(
